b51cd6e50ca663e4bc57f791a2df686079f29f0f
Ubuntu <ubuntu@ip-172-31-39-157.ec2.internal>
2023-12-09 17:50:23 +0000

    my updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, this is a simple Node.js application!');
+    res.send('Hello, this is an update message simple Node.js application!');
 });
 
 app.listen(port, () => {
