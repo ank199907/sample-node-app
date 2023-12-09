685657c7f504e1d75b073c17f8c0ca1a7e443fd6
Ubuntu <ubuntu@ip-172-31-39-157.ec2.internal>
2023-12-09 14:05:42 +0000

    Add Node.js application

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, this is a simple Node.js application!');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://52.90.243.174:${port}`);
+});
+
